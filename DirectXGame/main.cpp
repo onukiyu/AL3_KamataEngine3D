e4e3bd16a3e6632e324a29333faa5d6135f225e5
onukiyu <k023g0017@g.neec.ac.jp>
2024-04-12 11:53:02 +0900

    ブランチテスト

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2D_03_オオヌキ_ユウ_AL3");
+	win->CreateGameWindow(L"LE2D_03_オオヌキ_ユウ_AL3test");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
